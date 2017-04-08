0d4aa359fcf25b68f8e30e3becfd33804d9bbff7
Ling Yuan <lingy@uber.com>
2017-04-08 05:14:37 -0700

    Ling's first commit

---
+++ model.py
@@ -0,0 +1,84 @@
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers.core import Flatten, Lambda, Dense, Activation, Dropout
+from keras.layers.convolutional import Conv2D
+from keras.layers import Cropping2D
+import numpy as np
+import cv2
+import csv
+from os import path
+
+# Input shape is (160, 320, 3)
+def nv_sdc():
+
+    model = Sequential()
+
+    # Normalization and cropping
+    model.add(Lambda(lambda x: (x / 255. - 0.5), input_shape=(160, 320, 3)))
+    model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160, 320, 3)))
+
+    # 3 5x5 convolutional layers and 2 3x3 convolutional layers
+    model.add(Conv2D(24, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(36, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(48, (5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+
+    # Flatten
+    model.add(Flatten())
+
+    # 3 fully connected layers
+    model.add(Dense(100))
+    model.add(Dropout(0.5))
+    model.add(Dense(50))
+    model.add(Dropout(0.5))
+    model.add(Dense(10))
+
+    # Regression model outputs only one value
+    model.add(Dense(1))
+
+    return model
+
+def load_data(dir):
+    print(' -- loading samples from {}'.format(dir))
+    with open(path.join(dir, 'driving_log.csv')) as f:
+        reader = csv.reader(f)
+        lines = [line for line in reader][1:]
+    images = [cv2.imread(path.join(dir, line[0])) for line in lines]
+    images = images + ([cv2.flip(image.copy(), 1) for image in images])
+    angles = [float(line[3]) for line in lines]
+    angles = angles + ([-angle for angle in angles])
+    print(' -- loaded {} x 2 samples'.format(len(images)//2))
+    return images, angles
+
+def load_all_data(dirs):
+    all_images, all_angles = [], []
+    for dir in dirs:
+        new_images, new_angles = load_data(dir)
+        all_images, all_angles = all_images + new_images, all_angles + new_angles
+    return np.array(all_images), np.array(all_angles)
+
+def train(recording_dirs, saved_model=None, save_to=None):
+    print('Loading...')
+    X_train, y_train = load_all_data(recording_dirs)
+
+    print('Using {} training examples...'.format(len(X_train)))
+
+    if saved_model:
+        print('Using model {}...'.format(saved_model))
+        m = load_model(saved_model)
+    else:
+        print('Using new model')
+        m = nv_sdc()
+        m.compile(loss='mse', optimizer='adam')
+
+    print('Training...')
+    m.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
+
+    if save_to:
+        print('Saving...')
+        m.save(save_to)
+        print(' -- saved to {}'.format(save_to))
+
+recording_dirs = ['data']
+train(recording_dirs, save_to='saved_models/set06/model_official_data.h5')
