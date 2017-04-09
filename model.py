19a0696067fbaf70880bf953a761025e0f21ccd1
Ling Yuan <lingy@uber.com>
2017-04-08 23:56:26 -0700

    only use 2 dropout layers, crop less

--- model.py
+++ model.py
@@ -37,7 +37,6 @@ def nv_sdc():
     model.add(Dense(50))
     model.add(Dropout(0.5))
     model.add(Dense(10))
-    model.add(Dropout(0.5))
 
     # Regression model outputs only one value
     model.add(Dense(1))
